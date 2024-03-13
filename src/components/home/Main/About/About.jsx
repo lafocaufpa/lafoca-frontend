8ef658bf91b20fbb13a2580a00b07406ab8b91c1
Wenderson Santos <wgustavo.dev@gmail.com>
2024-03-13 18:32:10 -0300

    seção de compromissos com accordion e menina do lafoca

--- src/components/home/Main/About/About.jsx
+++ src/components/home/Main/About/About.jsx
@@ -1,4 +1,17 @@
+import Image from 'next/image';
 import styles from './About.module.css';
+import Menina from '@images/sobre/menina.png';
+
+import IconVisao from '@images/sobre/icon-visao.svg';
+import IconMissao from '@images/sobre/icon-missao.svg';
+import IconValores from '@images/sobre/icon-valores.svg';
+import IconPesquisa from '@images/sobre/icon-linha_de_pesquisa.svg';
+
+import { Accordion } from './Accordion/Accordion';
+
+// import { ReactComponent as IconMissao } from '../../assets/img/sobre/icon-missao.svg';
+// import { ReactComponent as IconPesquisa } from '../../assets/img/sobre/icon-linha_de_pesquisa.svg';
+// import { ReactComponent as IconValores} from '../../assets/img/sobre/icon-valores.svg';
 
 export default function About () {
   return (
@@ -8,8 +21,64 @@ export default function About () {
         <p className='global__paragraph_text' data-aos="fade-up"
           data-aos-duration="2000">Fundado em 2017, o LAFocA é formado por alunos e professores, além de parceiros de outras instituições de ensino e empresas do mercado de TI. Nosso foco é o aluno, buscando contribuir para o seu desenvolvimento pessoal, acadêmico e profissional. Esperamos ser reconhecidos como um grupo referência na formação de profissionais qualificados.</p>
       </div>
-      <div>
-          nossos compromissos
+      <div className={`${styles.appointments}`}>
+        <div className={styles.appointments__menina} 
+          data-aos="fade-right"
+          data-aos-offset="300"
+          data-aos-easing="ease-in-sine">
+          <Image src={Menina}/>
+        </div>
+        <div className={styles.container__accordion}>
+          <h2> Compromissos que <br/> <span>desenvolvemos</span></h2>
+          <p className='global__paragraph_text'>Conheça os princípios que norteiam as atividades do laboratório e garantem a excelência nos projetos e resultados obtidos.
+          </p>
+          <div className={styles.accordion}>
+            <div className="accordion">
+              <Accordion
+                Icon={IconVisao}
+                title="VISÃO"
+                text="Ser reconhecido como um grupo de pesquisa referência na
+                    formação de profissionais qualificados."
+              />
+              <Accordion
+                Icon={IconMissao}
+                title="MISSÃO"
+                text="Propiciar um ambiente adequado que permita desenvolver
+                    pesquisas de qualidade, contribuindo para o desenvolvimento dos
+                    alunos."
+              />
+              <Accordion
+                Icon={IconPesquisa}
+                title="LINHA DE PESQUISA"
+                text={
+                  <>
+                            
+                                • Engenharia de Software e Metodologias Ágeis<br /><br />
+                                • Interação Humano-Computador (IHC) e User eXperience(UX)<br /><br />
+                                • Jogos e Gameficação<br /><br />
+                                • Marketing Digital e Midias Sociais<br /><br />
+                                • Programação e Testes de Software
+                            
+                  </>}
+              />
+              <Accordion
+                Icon={IconValores}
+                title="VALORES"
+                text={
+                  <>
+                    <strong>Comunicação</strong> – Compartilhar informações, buscando transmitir conhecimento e valores.<br />
+                    <strong>Proatividade</strong> – Tomar iniciativa e decisões na realização das suas atividades.<br />
+                    <strong>Colaboração</strong> – Trabalhar ou cooperar com as atividades de uma ou mais pessoas do grupo.<br />
+                    <strong>Protagonismo</strong> – Aceitar os méritos e resultados da realização das suas atividades.<br />
+                    <strong>Comprometimento</strong> – Empenhar seus esforços e dedicação nas atividades das quais participa.<br />
+                    <strong>Pertencimento</strong> – Fazer parte de um grupo, se identificando com a linha de pesquisa.
+                  </>
+                }
+              />
+            </div>
+          </div>
+          
+        </div>
       </div>
     </section>
   );
---
+++ src/components/home/Main/About/Accordion/Accordion.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import { useSpring, animated} from 'react-spring';
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import styles from './Accordion.module.css';
+import Image from 'next/image';
+
+export const Accordion = ({ title, text, Icon }) => {
+  const [open, setOpen] = useState(false);
+  const [iconOpen, setIconOpen] = useState(true);
+  let toggleHandler = () => {
+    setOpen(!open);
+  };
+
+  const openAnimation = useSpring({
+    from: { opacity: '0', maxHeight: '25px', },
+    to: { opacity: '1', maxHeight: open ? '310px' : '77px' },
+    onRest: () => { setIconOpen(!iconOpen); },
+    config: { duration: '100' }
+  });
+
+  const iconAnimation = useSpring({
+    from: {
+      transform: 'rotate(0deg)',
+    },
+    to: {
+      transform: open ? 'rotate(180deg)' : 'rotate(0deg)',
+    },
+
+    config: { duration: '120' }
+  });
+
+  const borderAn = useSpring({
+    from: {
+      borderBottomStyle: 'none',
+    },
+    to: {
+      borderBottomWidth: '1px',
+      borderBottomStyle: open ? 'solid' : 'none',
+      borderBottomColor: 'rgba(101, 101, 101, 0.5)',
+      paddingBottom: '7px'
+    },
+    config: { duration: '300' }
+  });
+
+
+  return (
+    <animated.div className={styles.accordion__item} style={openAnimation} data-aos="fade-up">
+      <animated.div className={styles.accordion__header} style={borderAn} onClick={toggleHandler} >
+        {/* <Icon /> */}
+        <Image src={Icon}/>
+        <h4>{title}</h4>
+        <animated.i style={iconAnimation}>
+          {iconOpen ? <RemoveIcon style={{ fontSize: 30 }} className={styles.iconAccordion} /> : <AddIcon style={{ fontSize: 30 }} className={styles.iconAccordion} />}
+        </animated.i>
+      </animated.div>
+      <p className={styles.accordion__content} style={{ whiteSpace: 'pre-line' }}>{text}</p>
+    </animated.div>
+  );
+};
