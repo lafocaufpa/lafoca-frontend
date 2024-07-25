13aea4fb27f9c7d7694c73a5ccf41c40b9e8bb8d
wenderson gustavo <wgustavo.dev@gmail.com>
2024-07-24 23:14:44 -0300

    Alteração na visualização de itens de navegação na página inicial

--- src/components/home/Main/Projects/CardProject.jsx
+++ src/components/home/Main/Projects/CardProject.jsx
@@ -1,25 +1,17 @@
 import Image from 'next/image';
 import styles from './CardProject.module.css';
-import stylesAnimate from '@components/home/Custom-Slider/Animate.module.css';
 import Link from 'next/link';
 
-export default function CardProject({title, image, description, link, durationFade}) {
+export default function CardProject({ title, image, description, link, durationFade }) {
   return (
-    <div data-aos="fade-up" data-aos-duration={durationFade} className={`${styles.card_project} ${stylesAnimate.hover_animate} `}>
-      
-      <div className={styles.card_content}>
-        
-        <div className={styles.card_content_img}>
-          <Image src={image} alt={'Artigos'} />
-        </div>
+    <div data-aos="fade-up" data-aos-duration={durationFade} className={styles.card_project}>
+      <div className={styles.card_content_img}>
+        <Image src={image} alt={title} layout="fill" objectFit="cover" />
       </div>
-
       <div className={styles.card_info}>
         <h4>{title}</h4>
-        <p>
-          {description}
-        </p>
-        <Link href={link} className={stylesAnimate.card_link}>
+        <p>{description}</p>
+        <Link href={link} className={styles.card_link}>
           Ver mais {'>'}
         </Link>
       </div>
