32f5c9c1e524f0906c8bbad09472aef6c021b6c1
wenderson gustavo <wgustavo.dev@gmail.com>
2024-07-31 15:55:16 -0300

    ajustes no menu hamburguer em telas mobile

--- src/components/home/Header/navBar/NavBar.jsx
+++ src/components/home/Header/navBar/NavBar.jsx
@@ -2,9 +2,13 @@ import Logo from '@home/Logo/Logo';
 import styles from './NavBar.module.css';
 import Link from 'next/link';
 import { useState } from 'react';
+import useHash from '@/hooks/useHash';
+import arrow from '@images/icons/arrou-hamburguer.svg';
+import Image from 'next/image';
 
 export default function NavBar() {
   const [isActive, setIsActive] = useState(false);
+  const hash = useHash();
 
   const toggleMenu = () => {
     setIsActive(!isActive);
@@ -22,15 +26,19 @@ export default function NavBar() {
           <div className={styles.hamburguerLine}></div>
           <Link href="#sobre" onClick={closeMenu}>
             SOBRE
+            {hash === 'sobre' && <Image className={styles.arrow} src={arrow}/>}
           </Link>
           <Link href="#equipe" onClick={closeMenu}>
             EQUIPE
+            {hash === 'equipe' && <Image className={styles.arrow} src={arrow}/>}
           </Link>
           <Link href="#resultados" onClick={closeMenu}>
             RESULTADOS
+            {hash === 'resultados' && <Image className={styles.arrow} src={arrow}/>}
           </Link>
           <Link href="#pesquisas" onClick={closeMenu}>
             PESQUISAS
+            {hash === 'pesquisas' && <Image className={styles.arrow} src={arrow}/>}
           </Link>
         </nav>
         <button className={styles.MenuHamburger} onClick={toggleMenu}></button>
