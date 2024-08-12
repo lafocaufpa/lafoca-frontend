33f9f640f50e116e6f54af54372e07ada274274a
wenderson gustavo <wgustavo.dev@gmail.com>
2024-08-12 17:46:44 -0300

    remoção de avisos no console do navegador

--- src/components/home/Main/Projects/CardProject.jsx
+++ src/components/home/Main/Projects/CardProject.jsx
@@ -7,7 +7,7 @@ export default function CardProject({ title, image, description, link, durationF
     <div data-aos="fade-up" data-aos-duration={durationFade} className={styles.card_project}>
       <Link href={link} className={styles.card_link}>
         <div className={styles.card_content_img}>
-          <Image src={image} alt={title} fill style={{objectFit:'cover'}} quality={100} />
+          <Image src={image} alt={title} fill quality={100} sizes='auto'/>
         </div>
         <div className={styles.card_info}>
           <h4>{title}</h4>
